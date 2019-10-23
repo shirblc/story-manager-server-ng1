c7cc62beeb37db339cc331bf6a6ddffb7d2a2afd
Shir Bar Lev <shirblc@gmail.com>
2019-10-23 20:23:59 +0300

    added library manager functionality
    *get story details for all stories
    *get/set selected story
    *injected the librarian service

--- js/controllers/libraryMgr.js
+++ js/controllers/libraryMgr.js
@@ -1,6 +1,28 @@
 //library controller
 //contains all the stotries' basic data
 angular.module("StoryManager")
-	.controller('libraryMgr', function() {
+	.controller('libraryMgr', ['librarian', function(librarian) {
+	var numStories = librarian.getNumStories();
+	var storiesDetails = [];
+	var selectedStory = 0;
 	
-});
+	function getStoryDetails()
+	{
+		for(var i = 0; i < numStories; i++)
+		{
+			var storyDetails = librarian.getStory(i+1);
+			var story = { title: storyDetails.title, synopsis: storyDetails.synopsis };
+			storiesDetails.push(story);
+		}
+	}
+	
+	function getSelectedStory()
+	{
+		return selectedStory;
+	}
+	
+	function setSelectedStory(numSelected)
+	{
+		selectedStory = numSelected;
+	}
+}]);
