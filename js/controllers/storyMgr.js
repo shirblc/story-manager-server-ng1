d5b2ec1d7f52e1e82c8f149c00eb0aaa6612611e
Shir Bar Lev <shirblc@gmail.com>
2019-10-23 20:26:37 +0300

    added story manager functionality
    *edit story name
    *edit chapter details
    *add chapter
    *delete chapter

--- js/controllers/storyMgr.js
+++ js/controllers/storyMgr.js
@@ -1,5 +1,37 @@
 //new controller for the story manager template
 angular.module('StoryManager')
-	.controller('storyMgr', function() {
+	.controller('storyMgr', ['librarian', function(librarian) {
+		let storyDetails = librarian.getCurrentStory();
+		let storyName = storyDetails.name;
+		let storySynopsis = storyDetails.synopsis;
+		let chapters = storyDetails.chapters;
 		
-});
+		function changeStoryName(newName)
+		{
+			storyName = newName;
+		}
+		
+		function changeChapterName(chapterNum, chapterName)
+		{
+			chapters[chapterNum-1].name = chapterName;
+			librarian.updateStory(chapters);
+		}
+		
+		function changeChapterSynopsis(chapterNum, chapterSynopsis)
+		{
+			chapters[chapterNum-1].name = chapterSynopsis;
+			librarian.updateStory(chapters);
+		}
+		
+		function addChapter(chapterName, chapterSynopsis)
+		{
+			chapters.push({number: chapters.length, title: chapterName, synopsis: chapterSynopsis});
+			librarian.updateStory(chapters);
+		}
+		
+		function removeChapter(chapterID)
+		{
+			chapters.splice(chapterID, 1);
+			librarian.updateStory(chapters);
+		}
+}]);
