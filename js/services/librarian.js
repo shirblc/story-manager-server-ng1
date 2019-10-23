443ff0ec2ce1bdf16a3c3fa93b30c914c8c883bc
Shir Bar Lev <shirblc@gmail.com>
2019-10-23 21:33:25 +0300

    added functionality to the librarian service as used in controllers
    in accordance with libraryMgr:
    *getStoryDetails -> getStory
    *added getNumStories
    
    in accordance with storyMgr:
    *added updateStory
    *currentStory and currentStoryNum properties
    
    also added setCurrentStoryNum and updateStoryDetails

--- js/services/librarian.js
+++ js/services/librarian.js
@@ -2,19 +2,46 @@
 angular.module('StoryManager')
 	.service('librarian', function() {
 	var myStories = getStories();
+	var currentStoryNum = 0;
+	var currentStory = myStories[currentStoryNum];
 	
 		function getStories()
 		{
 			return $.getJSON('data/stories.json');
 		}
 	
-		function getStoryDetails(storyID)
+		function getStory(storyID)
 		{
-			return myStories[storyID];
+			return myStories.stories[storyID];
+		}
+		
+		function getNumStories()
+		{
+			return myStories.stories.length;
+		}
+		
+		function getCurrentStory()
+		{
+			return currentStory;
 		}
 	
-		function getChapterDetails(storyID, chapterID)
+		function setCurrentStoryNum(selectedStory)
 		{
-			
+			currentStoryNum = selectedStory;
+		}
+		
+		function updateStory(chapters)
+		{	myStories.stories[currentStory].chapters = chapters;
+			$.ajax({
+				url: "data/stories.json",
+				type: "POST",
+				data: myStories.stories[currentStory]
+			});
+		}
+		
+		function updateStoryDetails(storyName, storySynopsis)
+		{
+			myStories.stories[currentStory].name = storyName;
+		 	myStories.stories[currentStory].synopsis = storySynopsis;
 		}
 });
