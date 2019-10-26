9a84ba10d9e2ab48b350420c047ba503c4fa3bc4
Shir Bar Lev <shirblc@gmail.com>
2019-10-26 17:31:55 +0300

    Rewrote the Gulp file to fit Gulp 4.
    *All tasks except "serve" have been switched to functions and exported
    *Replaced "watcher" with "watchFiles"
    *Gulp.watch now works properly

--- gulpfile.js
+++ gulpfile.js
@@ -10,85 +10,116 @@ const order = require("gulp-order");
 const browserSync = require("browser-sync").create();
 
 //copies the html to the disribution folder
-gulp.task("copy-html", function() {
-	return gulp.src("index.html")
-	.pipe(gulp.dest('./dist'))
-});
+function copyHtml()
+{
+	return gulp
+		.src("index.html")
+		.pipe(gulp.dest("./dist"));
+};
 
 //copies the views to the distribution folder
-gulp.task("copy-views", function() {
-	return gulp.src("views/*")
-	.pipe(gulp.dest("./dist/views"))
-});
+function copyViews()
+{
+	return gulp
+		.src("views/*")
+		.pipe(gulp.dest("./dist/views"));
+}
+
 
 //copies the images folder to the distribution folder
-gulp.task("copy-imgs", function() {
-	return gulp.src("img/*")
-	.pipe(gulp.dest("dist/img"))
-});
+function copyImgs()
+{
+	return gulp
+		.src("img/*")
+		.pipe(gulp.dest("dist/img"));
+}
 
 //sets gulp to add prefixes with Autoprefixer after Dreamweaver outputs the Sass filee to CSS
 //once the prefixer finishes its job, outputs the file to the distribution folder
-gulp.task("styles", function() {
-	return gulp.src("css/*.css")
-	.pipe(postcss([autoprefixer()]))
-	.pipe(gulp.dest('./dist/css'))
-});
+function styles()
+{
+	return gulp
+		.src("css/*.css")
+		.pipe(postcss([autoprefixer()]))
+		.pipe(gulp.dest("./dist/css"));
+}
 
-gulp.task("copy-fonts", function() {
-	return gulp.src("css/*.ttf")
-	.pipe(gulp.dest("./dist/css"))
-});
+//copies the fonts to the distribution folder
+function copyFonts()
+{
+	return gulp
+		.src("css/*.ttf")
+		.pipe(gulp.dest("./dist/css"));
+}
 
 //deals with concating the scripts while in development mode
-gulp.task("scripts", function() {
-	return gulp.src("js/*.js")
-	.pipe(sourcemaps.init())
-	.pipe(babel({presets: ['@babel/preset-env']}))
-	.pipe(order([
-		"/js/app.js",
-		"/js/services/librarian.js",
-		"/js/libraryCtrl.js",
-		"/js/storyCtrl.js",
-		"/js/settingsCtrl.js"
-	]))
-	.pipe(concat('all.js'))
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest("dist/js"))
-});
+function scripts()
+{
+	return gulp
+		.src("js/**/*js")
+		.pipe(sourcemaps.init())
+		.pipe(babel({presets: ['@babel/preset-env']}))
+		.pipe(order([
+			"js/app.js", 
+			"js/services/librarian.js", 
+			"js/libraryCtrl.js", 
+			"js/storyCtrl.js", "js/settingsCtrl.js"]))
+		.pipe(concat("all.js"))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest("dist/js"));
+}
 
 //deals with concating the scripts while in production mode
-gulp.task("scripts-dist", function() {
-	return gulp.src("js/*.js")
-	.pipe(sourcemaps.init())
-	.pipe(babel({presets: ['@babel/preset-env']}))
-	.pipe(order([
-		"/js/app.js",
-		"/js/services/librarian.js",
-		"/js/libraryCtrl.js",
-		"/js/storyCtrl.js",
-		"/js/settingsCtrl.js"
-	]))
-	.pipe(concat('all.js'))
-	.pipe(uglify())
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest("dist/js"))
-});
+function scriptsDist()
+{
+	return gulp
+		.src("js/**/*js")
+		.pipe(sourcemaps.init())
+		.pipe(babel({presets: ['@babel/preset-env']}))
+		.pipe(order([
+			"js/app.js", 
+			"js/services/librarian.js", 
+			"js/libraryCtrl.js", 
+			"js/storyCtrl.js", "js/settingsCtrl.js"]))
+		.pipe(concat("all.js"))
+		.pipe(uglify())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest("dist/js"));
+}
 
 //automatic testing in the Jasmine headless browser
-gulp.task("tests", function() {
-	gulp.src(["dist/js/all.js", "tests/specs.js"])
-	.pipe(jasmineBrowser.specRunner({ console: true }))
-	.pipe(jasmineBrowser.headless({ driver: 'chrome' }));
-});
+function jasmineBrowserTest()
+{
+	return gulp
+		.src(["dist/js/all.js", "tests/specs.js"])
+		.pipe(jasmineBrowser.specRunner({ console: true }))
+		.pipe(jasmineBrowser.headless({ driver: "chrome" }));
+}
 
 //testing in whatever browser you want to use; just enter "localhost:3001" in the address line
-gulp.task("browser-tests", function() {
-	gulp.src(["dist/js/all.js", "tests/specs.js"])
-	.pipe(jasmineBrowser.specRunner())
-	.pipe(jasmineBrowser.server({ port: 3001 }));
-});
+function browserTests()
+{
+	return gulp
+		.src(["dist/js/all.js", "tests/specs.js"])
+		.pipe(jasmineBrowser.specRunner())
+		.pipe(jasmineBrowser.server({ port: 3001 }));
+}
 
+//prepare for distribution
+function dist()
+{
+	return gulp
+		.parallel(
+			copyHtml,
+			copyViews,
+			copyImgs, 
+			copyFonts, 
+			styles, 
+			scriptsDist
+		);
+}
+
+//boot up the server
 gulp.task("serve", function() {
 	browserSync.init({
 		server: {
@@ -97,24 +128,26 @@ gulp.task("serve", function() {
 	});
 });
 
-//watcher task to watch files
-gulp.task("watcher", gulp.parallel("styles", "copy-html", "copy-views", "copy-fonts"), function() {
-	gulp.watch('css/*.css', ['styles']);
-	gulp.watch('css/*.ttf', ['copy-fonts']);
-	gulp.watch('/index.html', ['copy-html']);
-	gulp.watch('/views/*', ['copy-views']);
-});
+//watch files for changes
+function watchFiles()
+{
+	gulp.watch('/index.html', copyHtml);
+	gulp.watch('views/*', copyViews);
+	gulp.watch('img/*', copyImgs);
+	gulp.watch('css/*.css', styles);
+	gulp.watch('css/*.ttf', copyFonts);
+	gulp.watch('/js/**/*.js', scripts);
+}
 
-//prepare for distribution
-gulp.task("dist", gulp.parallel(
-	"copy-html",
-	"copy-imgs",
-	"styles",
-	"scripts-dist"
-));
-
-//default task
-gulp.task("default", gulp.parallel("dist", "copy-html", "copy-imgs", "styles", "scripts", "scripts-dist", "tests", "browser-tests", "serve"), function() {
-	gulp.watch('css/*.css', ['styles']);
-	gulp.watch('/index.html', ['copy-html']);
-});
+//exports for gulp to recognise them as tasks
+exports.copyHtml = copyHtml;
+exports.copyViews = copyViews;
+exports.copyImgs = copyImgs;
+exports.styles = styles;
+exports.copyFonts = copyFonts;
+exports.scripts = scripts;
+exports.scriptsDist = scriptsDist;
+exports.jasmineBrowserTest = jasmineBrowserTest;
+exports.browserTests = browserTests;
+exports.dist = dist;
+exports.watchFiles = watchFiles;
