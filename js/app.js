defa10f4a50ced9e57159de2b2641c0fe92384bb
Shir Bar Lev <shirblc@gmail.com>
2020-03-13 16:29:30 +0200

    Changed the data folder to server to contain the web server files.

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ angular
 			loadData: function($http) {
 				return $http({
 					method: 'GET',
-					url: '/data/stories.json'
+					url: '/server/stories.json'
 				}).then(function(response) {
 					return response.data.stories;
 				});
@@ -35,7 +35,7 @@ angular
 			loadData: function($http) {
 				return $http({
 					method: 'GET',
-					url: '/data/stories.json'
+					url: '/server/stories.json'
 				}).then(function(response) {
 					return response.data.stories;
 				});
@@ -52,7 +52,7 @@ angular
 			loadData: function($http) {
 				return $http({
 					method: 'GET',
-					url: '/data/stories.json'
+					url: '/server/stories.json'
 				}).then(function(response) {
 					return response.data.stories;
 				});
@@ -70,7 +70,7 @@ angular
 			loadData: function($http) {
 				return $http({
 					method: 'GET',
-					url: '/data/stories.json'
+					url: '/server/stories.json'
 				}).then(function(response) {
 					return response.data.stories;
 				});
