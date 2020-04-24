65068f7c63f07437d156be4dab5fbd427a640fdb
Shir Bar Lev <shiruchii@gmail.com>
2020-04-24 15:51:49 +0300

    Added common error handlers.

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -38,6 +38,15 @@ def create_app():
 
     # Error Handlers
     # -----------------------------------------------------------------
+    # Bad request error handler
+    @app.errorhandler(400)
+    def bad_request(error):
+        jsonify({
+            'success': False,
+            'code': 400,
+            'message': 'Bad request. Check your request format.'
+        }), 400
+
     # Not found error handler
     @app.errorhandler(404)
     def not_found(error):
@@ -46,6 +55,24 @@ def create_app():
             'code': 404,
             'message': 'The resource you were looking for cannot be found.'
             }), 404
+
+    # Unprocessable error handler
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            'success': False,
+            'code': 422,
+            'message': 'Unprocessable request.'
+            }), 422
+
+    # Internal server error handler
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({
+            'success': False,
+            'code': 500,
+            'message': 'An internal server error occurred.'
+        }), 500
 
 
     return app
