937b70a99189867600a3f0f4cd874fc0ad65db52
Shir Bar Lev <shiruchii@gmail.com>
2020-04-24 15:30:48 +0300

    Added story route.

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
+
+from models import db_setup, Story, Chapter
 
 # Create Flask app
 def create_app():
@@ -11,6 +13,22 @@ def create_app():
     def index():
         return jsonify({
             'success': True
+        })
+
+    # Story route
+    @app.route('/story/<story_id>')
+    def get_story(story_id):
+        story = Story.query.filter(Story.id == story_id).one_or_none()
+
+        # If a story with this ID doesn't exist
+        if(story is None):
+            abort(404)
+        else:
+            formatted_story = story.format()
+
+        return jsonify({
+            'success': True,
+            'story': formatted_story
         })
 
 
