d46962d447b4a5535dc710bf580430170721fc12
Shir Bar Lev <shiruchii@gmail.com>
2020-04-24 15:29:37 +0300

    Added method to get formatted stories / chapters.

--- backend/models.py
+++ backend/models.py
@@ -19,6 +19,25 @@ class Story(db.Model):
     synopsis = db.Column(db.String(240))
     chapters = db.relationship('Chapter', backref='story_id')
 
+    # Get formatted story
+    def format(self):
+        story = {}
+        story['id'] = self.id
+        story['title'] = self.title
+        story['synopsis'] = self.synopsis
+        story['chapters'] = []
+
+        # Format the story's chapters
+        for chapter in self.chapters:
+            story['chapters'].append({
+                'id': chapter.id,
+                'number': chapter.number,
+                'title': chapter.title,
+                'synopsis': chapter.synopsis
+            })
+
+        return story
+
 # Chapter Model
 class Chapter(db.Model):
     __tablename__ = 'chapters'
@@ -27,3 +46,13 @@ class Chapter(db.Model):
     title = db.Column(db.String(120), nullable=False)
     synopsis = db.Column(db.String(240))
     story_id = db.Column(db.Integer, db.ForeignKey('stories.id'))
+
+    # Get formatted chapter
+    def format(self):
+        chapter = {}
+        chapter['id'] = self.id
+        chapter['number'] = self.number
+        chapter['title'] = self.title
+        chapter['synopsis'] = self.synopsis
+
+        return chapter
