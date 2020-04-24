6dcb0d762f38da150bcffe10903dd87b50bd0611
Shir Bar Lev <shiruchii@gmail.com>
2020-04-24 15:14:39 +0300

    Added home route.

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -4,4 +4,14 @@ from flask import Flask, request, jsonify
 def create_app():
     app = Flask('__name__')
 
+    # Routes
+    # -----------------------------------------------------------------
+    # Home route
+    @app.route('/')
+    def index():
+        return jsonify({
+            'success': True
+        })
+
+
     return app
